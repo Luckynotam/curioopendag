5b88c21ccdc7b7481a0d40a8392e021b746646a7
Luckynotam <l.w.t.maton@gmail.com>
2026-09-16 11:08:52 +0200

    First commit

---
+++ index.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="rsc/css/style.css">
+    <title>Curio | Software developer</title>
+
+</head>
+<body>
+<header>
+    <img src="rsc/media/Logos/curio-03-geel-logo-rgb.png" alt="Curio Logo">
+</header>
+<main>
+    <div class="top-container">
+        <div class="left-img">
+            <img src="rsc/media/Sanne%20Bas%20-%20Curio%20-%20Terheijdenseweg%20350-2%20(Klein).jpg" alt="">
+        </div>
+        <div class="right-text">
+            <h1>Software development?! Bij curio!</h1>
+            <p>Werk jij graag met computers en zou je hier graag het uiterste uit willen halen? Dan is de opleiding software development bij curio wat voor jou. Je werkt dagelijks met computers en leert hoe je van ontwerp tot applicatie komt. Je begeleidt klanten in het werkproces en leert met behulp van SCRUM te werken in een team, want zoals ze hier vaak zeggen "Teamwork makes the dream work!" Daanraast moet je ook erg flexibel zijn want ons vakgebeid verandert voortduren.  Dus als jij thuis bent in computers en je bereid bent de ruwe zeeën van het progameren te trotseren, dan is deze opleiding iets voor jou!</p>
+        </div>
+    </div>
+
+</main>
+<footer></footer>
+</body>
+</html>
